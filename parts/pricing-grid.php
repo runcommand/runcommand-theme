5040ed52ca0f631e8c38ae9bac8e34737fba1f6f
Daniel Bachhuber <daniel@handbuilt.co>
2016-06-19 16:53:30 -0700

    Mention excerpts in the pricing grid

--- parts/pricing-grid.php
+++ parts/pricing-grid.php
@@ -10,6 +10,7 @@
 			<li class="title">Gold</li>
 			<li class="price"><del>$4500</del><br />$2250</li>
 			<li class="bullet-item">1 day response time</li>
+			<li class="bullet-item">Unlimited access to <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
 		</ul>
 	</li>
 	<li>
@@ -17,6 +18,7 @@
 			<li class="title">Silver</li>
 			<li class="price"><del>$1500</del><br />$750</li>
 			<li class="bullet-item">3 day response time</li>
+			<li class="bullet-item">Unlimited access to <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
 		</ul>
 	</li>
 	<li>
