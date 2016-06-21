cf870e24e6bf50182deb3265b578b1a66f84086c
Daniel Bachhuber <daniel@handbuilt.co>
2016-06-21 09:26:55 -0700

    Add HelpScout beacon to the page

--- footer.php
+++ footer.php
@@ -101,5 +101,15 @@
 		</script>
 	<?php endif; ?>
 
+	<script>!function(e,o,n){window.HSCW=o,window.HS=n,n.beacon=n.beacon||{};var t=n.beacon;t.userConfig={},t.readyQueue=[],t.config=function(e){this.userConfig=e},t.ready=function(e){this.readyQueue.push(e)},o.config={docs:{enabled:!1,baseUrl:""},contact:{enabled:!0,formId:"6045f2d7-37ca-11e6-aae8-0a7d6919297d"}};var r=e.getElementsByTagName("script")[0],c=e.createElement("script");c.type="text/javascript",c.async=!0,c.src="https://djtflbt20bdde.cloudfront.net/",r.parentNode.insertBefore(c,r)}(document,window.HSCW||{},window.HS||{});</script>
+	<script>
+		HS.beacon.config({
+			icon: 'question',
+			color: '#fd0a00',
+			poweredBy: false,
+			attachment: false,
+		});
+	</script>
+
 </body>
 </html>
