1d0ec03e140eaf41d3d02ffaeb345ed872f27edd
Daniel Bachhuber <daniel@handbuilt.co>
2016-07-08 15:51:42 -0700

    Update the pricing chart again

--- parts/pricing-grid.php
+++ parts/pricing-grid.php
@@ -3,31 +3,33 @@
 		<ul class="pricing-table">
 			<li class="title">Silver</li>
 			<li class="price"><del>$1500</del><br />$750<br/><small>per month</small></li>
+			<li class="bullet-item">Silver-backing for <a href="<?php echo esc_url( home_url( 'sparks/' ) ); ?>">sparks</a></li>
 			<li class="bullet-item">2 day response time</li>
 			<li class="bullet-item">Unlimited support tickets</li>
-			<li class="bullet-item">Unlimited <a href="<?php echo esc_url( home_url( 'sparks/' ) ); ?>">sparks</a> and <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
+			<li class="bullet-item">Unlimited access to <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
 		</ul>
 	</li>
 	<li>
 		<ul class="pricing-table">
 			<li class="title">Gold</li>
 			<li class="price"><del>$4500</del><br />$2250<br/><small>per month</small></li>
+			<li class="bullet-item">Gold-backing for <a href="<?php echo esc_url( home_url( 'sparks/' ) ); ?>">sparks</a></li>
 			<li class="bullet-item">2 day response time</li>
 			<li class="bullet-item">Unlimited support tickets</li>
 			<li class="bullet-item">Bi-weekly videoconference</li>
-			<li class="bullet-item">Unlimited <a href="<?php echo esc_url( home_url( 'sparks/' ) ); ?>">sparks</a> and <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
+			<li class="bullet-item">Unlimited access to <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
 		</ul>
 	</li>
 	<li>
 		<ul class="pricing-table">
 			<li class="title">Platinum</li>
 			<li class="price"><del>$14000</del><br />$7000<br/><small>per month</small></li>
+			<li class="bullet-item">Platinum-backing for <a href="<?php echo esc_url( home_url( 'sparks/' ) ); ?>">sparks</a></li>
 			<li class="bullet-item">1 day response time</li>
 			<li class="bullet-item">Unlimited support tickets</li>
 			<li class="bullet-item">Weekly videoconferences</li>
-			<li class="bullet-item">Private Slack channel</li>
-			<li class="bullet-item">Private Github / Bitbucket code review</li>
-			<li class="bullet-item">Unlimited <a href="<?php echo esc_url( home_url( 'sparks/' ) ); ?>">sparks</a> and <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
+			<li class="bullet-item">runcommand presence on your Slack, Github, or internal systems</li>
+			<li class="bullet-item">Unlimited access to <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
 		</ul>
 	</li>
 </ul>
