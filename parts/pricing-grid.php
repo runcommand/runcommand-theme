10a349e4eb856a761ca016462ced9a644662b0a9
Daniel Bachhuber <daniel@handbuilt.co>
2016-07-08 15:02:50 -0700

    First pass at refining the pricing table

--- parts/pricing-grid.php
+++ parts/pricing-grid.php
@@ -1,38 +1,33 @@
-<ul class="medium-block-grid-4 pricing-grid">
-	<li>
-		<ul class="pricing-table">
-			<li class="title">Bronze</li>
-			<li class="price">TBA</li>
-			<li class="bullet-item">Basic response time</li>
-			<li class="bullet-item">Unlimited access to <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
-		</ul>
-	</li>
+<ul class="medium-block-grid-3 pricing-grid">
 	<li>
 		<ul class="pricing-table">
 			<li class="title">Silver</li>
-			<li class="price"><del>$1500</del><br />$750</li>
-			<li class="bullet-item">3 day response time</li>
-			<li class="bullet-item">Unlimited access to <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
+			<li class="price"><del>$1500</del><br />$750<br/><small>per month</small></li>
+			<li class="bullet-item">2 day response time</li>
+			<li class="bullet-item">Unlimited support tickets</li>
+			<li class="bullet-item">Unlimited <a href="<?php echo esc_url( home_url( 'sparks/' ) ); ?>">sparks</a> and <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
 		</ul>
 	</li>
 	<li>
 		<ul class="pricing-table">
 			<li class="title">Gold</li>
-			<li class="price"><del>$4500</del><br />$2250</li>
-			<li class="bullet-item">1 day response time</li>
-			<li class="bullet-item">Monthly videoconference consultation</li>
-			<li class="bullet-item">Unlimited access to <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
+			<li class="price"><del>$4500</del><br />$2250<br/><small>per month</small></li>
+			<li class="bullet-item">2 day response time</li>
+			<li class="bullet-item">Unlimited support tickets</li>
+			<li class="bullet-item">Bi-weekly videoconference</li>
+			<li class="bullet-item">Unlimited <a href="<?php echo esc_url( home_url( 'sparks/' ) ); ?>">sparks</a> and <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
 		</ul>
 	</li>
 	<li>
 		<ul class="pricing-table">
 			<li class="title">Platinum</li>
-			<li class="price">TBA</li>
-			<li class="bullet-item">Prioritized response time</li>
-			<li class="bullet-item">Weekly videoconference consultation</li>
+			<li class="price"><del>$14000</del><br />$7000<br/><small>per month</small></li>
+			<li class="bullet-item">1 day response time</li>
+			<li class="bullet-item">Unlimited support tickets</li>
+			<li class="bullet-item">Weekly videoconferences</li>
 			<li class="bullet-item">Private Slack channel</li>
-			<li class="bullet-item">Private Github code review</li>
-			<li class="bullet-item">Unlimited access to <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
+			<li class="bullet-item">Private Github / Bitbucket code review</li>
+			<li class="bullet-item">Unlimited <a href="<?php echo esc_url( home_url( 'sparks/' ) ); ?>">sparks</a> and <a href="<?php echo esc_url( home_url( 'excerpts/' ) ); ?>">excerpts</a></li>
 		</ul>
 	</li>
 </ul>
