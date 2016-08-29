837ad012f92ff127fd673a35806e96c418e7b191
Daniel Bachhuber <daniel@handbuilt.co>
2016-08-29 07:40:31 -0700

    Add Memberful snippet to the `<head>`

---
+++ parts/header/memberful.php
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+  window.MemberfulOptions = {site: "https://runcommand.memberful.com"};
+
+  (function() {
+    var s   = document.createElement('script');
+
+    s.type  = 'text/javascript';
+    s.async = true;
+    s.src   = 'https://d35xxde4fgg0cx.cloudfront.net/assets/embedded.js';
+
+    setup = function() { window.MemberfulEmbedded.setup(); }
+
+    if(s.addEventListener) { s.addEventListener("load", setup, false); } else { s.attachEvent("onload", setup); }
+
+    ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+  })();
+</script>
