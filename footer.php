75fb74fb28fc268a1d4e1f414d2e8e9290af707e
Daniel Bachhuber <daniel@handbuilt.co>
2016-10-12 05:30:23 -0700

    Remove Intercom from the page. Bye bye

--- footer.php
+++ footer.php
@@ -95,14 +95,6 @@
 		</script>
 	<?php endif; ?>
 
-	<script>
-		window.intercomSettings = {
-			app_id: "w8jbky24"
-		};
-	</script>
-
-	<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/w8jbky24';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
-
 	<script async>window.twttr = (function(d, s, id) {
 		var js, fjs = d.getElementsByTagName(s)[0],
 		t = window.twttr || {};
