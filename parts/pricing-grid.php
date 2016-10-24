561eadc473b82a107f42c262c2acfcc500649a9f
Daniel Bachhuber <daniel@handbuilt.co>
2016-10-24 06:22:58 -0700

    Tweak pricing table layout

--- parts/pricing-grid.php
+++ parts/pricing-grid.php
@@ -1,18 +1,6 @@
 <ul class="medium-block-grid-2 pricing-grid">
 	<li>
 		<ul class="pricing-table">
-			<li class="title">Silver</li>
-			<li class="price">$599<br/><small>per quarter</small></li>
-			<li class="bullet-item"><a href="<?php echo esc_url( home_url( 'wp/profile/' ) ); ?>">wp profile</a>, <a href="<?php echo esc_url( home_url( 'wp/doctor/' ) ); ?>">wp doctor</a></li>
-			<li class="bullet-item">WP-CLI operational support</li>
-			<li class="bullet-item">Best-effort response time</li>
-			<li class="bullet-item">3 support seats</li>
-			<li class="bullet-item">1 Github-based developer seat</li>
-			<li class="cta-button"><a class="button" href="https://runcommand.typeform.com/to/BpblUg">Sign Up</a></li>
-		</ul>
-	</li>
-	<li>
-		<ul class="pricing-table">
 			<li class="title">Gold</li>
 			<li class="price">$2799<br /><small>per quarter</small></li>
 			<li class="bullet-item"><a href="<?php echo esc_url( home_url( 'wp/profile/' ) ); ?>">wp profile</a>, <a href="<?php echo esc_url( home_url( 'wp/doctor/' ) ); ?>">wp doctor</a>, and future premium commands</li>
@@ -22,6 +10,18 @@
 			<li class="bullet-item">3 Github-based developer seats</li>
 			<li class="bullet-item">runcommand presence in your Slack</li>
 			<li class="bullet-item">Quarterly video conference</li>
+			<li class="cta-button"><a class="button" href="https://runcommand.typeform.com/to/BpblUg">Sign Up</a></li>
+		</ul>
+	</li>
+	<li>
+		<ul class="pricing-table">
+			<li class="title">Silver</li>
+			<li class="price">$599<br/><small>per quarter</small></li>
+			<li class="bullet-item"><a href="<?php echo esc_url( home_url( 'wp/profile/' ) ); ?>">wp profile</a>, <a href="<?php echo esc_url( home_url( 'wp/doctor/' ) ); ?>">wp doctor</a></li>
+			<li class="bullet-item">WP-CLI operational support</li>
+			<li class="bullet-item">Best-effort response time</li>
+			<li class="bullet-item">3 support seats</li>
+			<li class="bullet-item">1 Github-based developer seat</li>
 			<li class="cta-button"><a class="button" href="https://runcommand.typeform.com/to/BpblUg">Sign Up</a></li>
 		</ul>
 	</li>
